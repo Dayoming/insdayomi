aecf506fbfbcf7e0b03d8e26111b8ca091dcee4c
Dayoming <jdayoung1204@naver.com>
2018-08-12 22:07:24 +0900

    /insda 경로 입력하지 않아도 index 화면 표시

--- insdayomi/urls.py
+++ insdayomi/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^insda/', include('insda.urls')),
+    url(r'', include('insda.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
