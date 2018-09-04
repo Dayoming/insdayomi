949ba7c0d1a22e5daafb0c1afc8846efa6ef4aa2
Dayoming <jdayoung1204@naver.com>
2018-09-05 03:28:12 +0900

    크롤링 모델 수정

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class CrawlerArticle(models.Model):
+    title = models.CharField(max_length=50)
+    language = models.CharField(max_length=50, default='한자')
+    photo = models.ImageField(blank=False, upload_to='crawler/%Y/%m/%d')
+    cdate = models.DateTimeField(auto_now_add=True)
